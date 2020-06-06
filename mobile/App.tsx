28467da7e689afec33b2eee62000aad107cc4e1c
MatteusGuedz <dev.guedz@gmail.com>
2020-06-06 14:09:41 -0400

    Mobile Atualizado Last

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { AppLoading} from 'expo';
+import Routes from './src/routes';
+
+import { Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+import { Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
+
+export default function App() {
+  const [fontsloading] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium
+  })
+
+
+  if (!fontsloading){
+    return <AppLoading />
+  }
+
+
+  return (
+
+    <> 
+    <StatusBar 
+      barStyle="dark-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    <Routes /> 
+    </>
+  );
+}
+
